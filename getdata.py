0a4dfa1540c8e049b13218cb7e911a61dae6573a
harli-lacej <harlilacej16@gmail.com>
2022-12-10 19:13:51 +0100

    Hot Swap functionality added

--- getdata.py
+++ getdata.py
@@ -34,7 +34,7 @@ def insert_varibles_into_table(deviceID,temperature):
         user=username,
         password=passwd,
         port=tcpip_port,
-        database=databasename
+        database=database_name
         )
         cursor = connection.cursor()
         #inserting values given in tha function as parameters
@@ -54,10 +54,10 @@ def insert_varibles_into_table(deviceID,temperature):
             cursor.close()
             connection.close()
 
-#creating a list to store addresses found
-address_list=[]
 #defining a function to add new addresses found in a list 
 def scan():
+    #creating a list to store addresses found
+    address_list=[]
     #creating a subprocess that uses as argument a command to detect i2c addresses in a bus
     p = subprocess.Popen(['i2cdetect', '-y','1'],stdout=subprocess.PIPE,)
     #defining all possible address that MCP8908 use
@@ -77,28 +77,30 @@ def scan():
     return address_list
 
 #calling the function scan
-devices=scan()
+device=scan()
 #Console output header section
-print(address_list)
+print(device)
+
 print("+----------+-------------+")
 print("| DeviceID | Temperature |")
 print("+----------+-------------+")
 
+devices = {}
 while True:
-    new_list = scan()
-    #checking if the first and second call of the scan function any differeces have
-    if devices != new_list:
-        if new_list - devices:
-            print("New devices:", new_list - devices)
-        if devices - new_list:
-            print("Gone devices:", devices - new_list)
-        devices = new_list
-        #if defferences where present,the program terminates(rerun with help of a bash script)
-        sys.exit()
+    time.sleep(1)
 
-    #creating for each sensor found a new object 
-    for i in range(0,len(new_list)):
-        globals()[f'sensor{i}']=MCP9808.MCP9808(int(new_list[i],16))
+    addresses = set(scan())
+    to_add = addresses - devices.keys()
+    to_remove = devices.keys() - addresses
+
+    for address in to_add:
+        devices[address] = MCP9808.MCP9808(int(address, 16))
+        print(f"Added new device {address}")
+
+    for address in to_remove:
+        del devices[address]
+        print(f"Removed device {address}. It was not found while scanning.")
+
 
     #customizing the the output table using prettytable
     t = PrettyTable(['DeviceID', 'Temperature'])
@@ -107,44 +109,42 @@ while True:
     t.hrules = 1
 
     #if a object called "sensor0" is created the code inside the if will be exectued
-    if 'sensor0' in globals():
+    if ' 18' in devices:
         #setting the resolution of temperature values
-        sensor0.set_resolution(3)
+        #sensor0.set_resolution(3)
         #defing a function to get data from the sensor,display them and insert in database
         def get_data():
-            i=0
-            for i in range(2):
-                #checking if we are dealing with a MCP9808 sensor
-                if(sensor0.begin() == True):
-                    #getting the tempertature from the sensor
-                    temp = sensor0.get_temp()
-                    #delay of 10 seconds 
-                    time.sleep(10)
-                    #inserting the temperature and DeviceID in the database 
-                    insert_varibles_into_table(1,temp)
-                    #adding a new row to prettytable
-                    t.add_row(['1',temp])
-                    print( "\n".join(t.get_string().splitlines()[-2:]))
+            device1 = devices[' 18']
+            #checking if we are dealing with a MCP9808 sensor
+            if(device1.begin() == True):
+                #getting the tempertature from the sensor
+                temp = device1.get_temp()
+                #delay of 10 seconds 
+                time.sleep(10)
+                #inserting the temperature and DeviceID in the database 
+                insert_varibles_into_table(1,temp)
+                #adding a new row to prettytable
+                t.add_row(['1',temp])
+                print( "\n".join(t.get_string().splitlines()[-2:]))
  
     #if a object called "sensor1" is created the code inside the if will be exectued                   
-    if 'sensor1' in globals():
+    if ' 19' in devices:
+        device2 = devices[' 19']
         #setting the resolution of temperature values
-        sensor1.set_resolution(3)
+        device2.set_resolution(3)
         #defing a function to get data from the sensor,display them and insert in database
         def get_data2():
-            a=0
-            for a in range(2):
-                #checking if we are dealing with a MCP9808 sensor
-                if(sensor1.begin() == True):
-                    #getting the tempertature from the sensor
-                    temp1 = sensor1.get_temp()
-                    #delay of 10 seconds 
-                    time.sleep(10)
-                    #inserting the temperature and DeviceID in the database 
-                    insert_varibles_into_table(2,temp1)
-                    #adding a new row to prettytable
-                    t.add_row(['2',temp1])
-                    print( "\n".join(t.get_string().splitlines()[-2:]))
+            #checking if we are dealing with a MCP9808 sensor
+            if(device2.begin() == True):
+                #getting the tempertature from the sensor
+                temp1 = device2.get_temp()
+                #delay of 10 seconds 
+                time.sleep(10)
+                #inserting the temperature and DeviceID in the database 
+                insert_varibles_into_table(2,temp1)
+                #adding a new row to prettytable
+                t.add_row(['2',temp1])
+                print( "\n".join(t.get_string().splitlines()[-2:]))
                     
     #if a object called "sensor2" is created the code inside the if will be exectued                    
     if 'sensor2' in globals():
@@ -152,19 +152,17 @@ while True:
         sensor2.set_resolution(3)
         #defing a function to get data from the sensor,display them and insert in database
         def get_data3():
-            b=0
-            for b in range(2):
-                #checking if we are dealing with a MCP9808 sensor
-                if(sensor2.begin() == True):
-                    #getting the tempertature from the sensor
-                    temp2 = sensor2.get_temp()
-                    #delay of 10 seconds 
-                    time.sleep(10)
-                    #inserting the temperature and DeviceID in the database 
-                    insert_varibles_into_table(3,temp2)
-                    #adding a new row to prettytable
-                    t.add_row(['3',temp2])
-                    print( "\n".join(t.get_string().splitlines()[-2:]))
+            #checking if we are dealing with a MCP9808 sensor
+            if(sensor2.begin() == True):
+                #getting the tempertature from the sensor
+                temp2 = sensor2.get_temp()
+                #delay of 10 seconds 
+                time.sleep(10)
+                #inserting the temperature and DeviceID in the database 
+                insert_varibles_into_table(3,temp2)
+                #adding a new row to prettytable
+                t.add_row(['3',temp2])
+                print( "\n".join(t.get_string().splitlines()[-2:]))
                 
     #if a object called "sensor3" is created the code inside the if will be exectued      
     if 'sensor3' in globals():
@@ -172,28 +170,24 @@ while True:
         sensor3.set_resolution(3)
         #defing a function to get data from the sensor,display them and insert in database
         def get_data4():
-            c=0
-            for c in range(2):
-                #checking if we are dealing with a MCP9808 sensor
-                if(sensor3.begin() == True):
-                    #getting the tempertature from the sensor
-                    temp3 = sensor3.get_temp()
-                    #delay of 10 seconds 
-                    time.sleep(10)
-                    #inserting the temperature and DeviceID in the database 
-                    insert_varibles_into_table(4,temp3)
-                     #adding a new row to prettytable
-                    t.add_row(['4',temp3])
-                    print( "\n".join(t.get_string().splitlines()[-2:]))
+            #checking if we are dealing with a MCP9808 sensor
+            if(sensor3.begin() == True):
+                #getting the tempertature from the sensor
+                temp3 = sensor3.get_temp()
+                #delay of 10 seconds 
+                time.sleep(10)
+                #inserting the temperature and DeviceID in the database 
+                insert_varibles_into_table(4,temp3)
+                    #adding a new row to prettytable
+                t.add_row(['4',temp3])
+                print( "\n".join(t.get_string().splitlines()[-2:]))
                     
     #if a object called "sensor4" is created the code inside the if will be exectued  
     if 'sensor4' in globals():
     #setting the resolution of temperature values
-    sensor4.set_resolution(3)
-    #defing a function to get data from the sensor,display them and insert in database
-    def get_data5():
-        d=0
-        for d in range(2):
+        sensor4.set_resolution(3)
+        #defing a function to get data from the sensor,display them and insert in database
+        def get_data5():
             #checking if we are dealing with a MCP9808 sensor
             if(sensor4.begin() == True):
                 #getting the tempertature from the sensor
@@ -209,11 +203,9 @@ while True:
     #if a object called "sensor5" is created the code inside the if will be exectued  
     if 'sensor5' in globals():
     #setting the resolution of temperature values
-    sensor5.set_resolution(3)
-    #defing a function to get data from the sensor,display them and insert in database
-    def get_data6():
-        e=0
-        for e in range(2):
+        sensor5.set_resolution(3)
+        #defing a function to get data from the sensor,display them and insert in database
+        def get_data6():
             #checking if we are dealing with a MCP9808 sensor
             if(sensor5.begin() == True):
                 #getting the tempertature from the sensor
@@ -228,12 +220,10 @@ while True:
     
     #if a object called "sensor6" is created the code inside the if will be exectued  
     if 'sensor6' in globals():
-    #setting the resolution of temperature values
-    sensor6.set_resolution(3)
-    #defing a function to get data from the sensor,display them and insert in database
-    def get_data7():
-        f=0
-        for f in range(2):
+        #setting the resolution of temperature values
+        sensor6.set_resolution(3)
+        #defing a function to get data from the sensor,display them and insert in database
+        def get_data7():
             #checking if we are dealing with a MCP9808 sensor
             if(sensor6.begin() == True):
                 #getting the tempertature from the sensor
@@ -245,15 +235,13 @@ while True:
                 #adding a new row to prettytable
                 t.add_row(['7',temp6])
                 print( "\n".join(t.get_string().splitlines()[-2:]))
-                
+                    
     #if a object called "sensor7" is created the code inside the if will be exectued  
     if 'sensor7' in globals():
-    #setting the resolution of temperature values
-    sensor7.set_resolution(3)
-    #defing a function to get data from the sensor,display them and insert in database
-    def get_data8():
-        g=0
-        for g in range(2):
+        #setting the resolution of temperature values
+        sensor7.set_resolution(3)
+        #defing a function to get data from the sensor,display them and insert in database
+        def get_data8():
             #checking if we are dealing with a MCP9808 sensor
             if(sensor7.begin() == True):
                 #getting the tempertature from the sensor
@@ -267,10 +255,12 @@ while True:
                 print( "\n".join(t.get_string().splitlines()[-2:]))
                 
                 
-
-    thread1 = threading.Thread(target=get_data, args=())
-    thread1.start()
-    thread2= threading.Thread(target=get_data2, args=())
-    thread2.start()
+    if ' 18' in devices:
+        thread1 = threading.Thread(target=get_data, args=())
+        thread1.start()
+    if ' 19' in devices:
+        thread2= threading.Thread(target=get_data2, args=())
+        thread2.start()
     thread1.join()
     thread2.join()
+
